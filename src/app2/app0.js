d116566cf7dc5330b7a7f70c2d50a4174fa301ab
alwayswannafly <15007159069@163.com>
2020-08-20 16:34:59 +0800

    文件系统操作详解,同步异步

--- src/app2/app0.js
+++ src/app2/app0.js
@@ -1,6 +1,23 @@
 /*
-*  Node操作文件系统通过原生模块来实现:fs
+*  Node操作文件系统通过原生模块来实现:fs;
+*  对于fs中的绝大多数qpi,Node都提供了相同功能的两个版本:同步版本和异步版本;
+*  xxxx(异步)
+*  xxxxSync(同步)
+*  尽最大可能去使用异步版本,不会阻塞主线程;
 */
 const fs = require('fs');
 
-fs.readFileSync()
+try {
+    const data = fs.readFileSync('test.txt', 'utf-8');
+    console.log(data);
+} catch (e) {
+    console.log(e);
+}
+
+fs.readFile('test.txt', {encoding: 'utf8', flag: 'r'}, function (error, data) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+})
