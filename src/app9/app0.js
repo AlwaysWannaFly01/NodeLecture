2c6356c93420699bd668d1b1ace4dd5e727f846d
alwayswannafly <15007159069@163.com>
2020-08-27 15:28:24 +0800

    koa详解与洋葱模型

---
+++ src/app9/app0.js
@@ -0,0 +1,31 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    console.log('myFunction started');
+    await next();
+    console.log('myFunction finished')
+});
+
+app.use(async (ctx, next) => {
+    console.log('myFunction2 started');
+    await next();
+    console.log('myFunction2 finished')
+});
+
+app.use(async (ctx, next) => {
+    console.log('myFunction3 started');
+    await next();
+    console.log('myFunction3 finished')
+});
+
+
+
+app.use(async ctx => {
+
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h2>hello</h2>';
+
+});
+
+app.listen(3000);
